4f8e2b40ca662794ba947d4c30ef89b83dd70fdb
Matheus Cristofoli <matfrietasc@gmail.com>
2024-02-24 21:24:58 -0500

    Book now shows if available or not

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,8 +2,8 @@ import { Link, useLoaderData, useSearchParams } from 'react-router-dom'
 
 const Books = () => {
   const { books } = useLoaderData()
-  const [searchParams] = useSearchParams()
   // get params from url to filter books
+  const [searchParams] = useSearchParams()
   document.title = 'Book Buddy'
 
   return (
@@ -17,7 +17,7 @@ const Books = () => {
             .map((book) => {
               return (
                 <div key={book.id} className='card'>
-                  <div className='max-w-sm overflow-hidden rounded shadow-lg'>
+                  <div className='relative max-w-sm overflow-hidden rounded shadow-lg'>
                     <Link to={`/book/${book.id}`}>
                       <img className='w-full' src={book.coverimage} alt={book.description} />
                     </Link>
@@ -25,11 +25,18 @@ const Books = () => {
                       <div className='mb-2 text-xl font-bold'>{book.title}</div>
                       <p className='text-base text-gray-700'>{book.description}</p>
                     </div>
-                    <div className='px-6 pt-4 pb-2'>
+                    <div className='px-6 pt-4 pb'>
                       <span className='inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full'>
                         Author: {book.author}
                       </span>
                     </div>
+                    <span
+                      className={`absolute bottom-0 right-0 px-4 py-2 rounded-tl-md text-gray-200 ${
+                        book.available ? 'bg-green-600' : 'bg-gray-500 '
+                      }`}
+                    >
+                      {book.available ? 'Available' : 'Reserved'}
+                    </span>
                   </div>
                 </div>
               )
