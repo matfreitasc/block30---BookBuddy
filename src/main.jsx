a8f1ad969e171226a5d01bd34a464192a7f0378b
Matheus Cristofoli <matfrietasc@gmail.com>
2024-02-25 21:07:02 -0500

    Did not work

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,6 @@ import { RouterProvider } from 'react-router-dom'
 import router from '@routes/router'
 import { AuthProvider } from '@context/AuthProvider'
 
-if (import.meta.env.PROD) {
-  // inject this script into the head of the document
-
-  const script = document.createElement('script')
-  script.text = `if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
-    __REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function() {};
-  }`
-  document.head.appendChild(script)
-}
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
